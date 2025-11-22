63e82edce84dd91f44591332cd0348efb79f9458
Igor <you@example.com>
2025-11-22 18:10:38 +0000

    ajustes

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 # %%
+from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 from sklearn import tree
-
-
 # %%
 
 spark = SparkSession.builder \
     .appName("MyApp") \
-        .config("spark.executor.memory", "2g") \
-.config("spark.driver.memory", "1g") \
-            .config("spark.sql.adaptive.enabled", "true") \
-        .getOrCreate()
+    .config("spark.executor.memory", "2g") \
+    .config("spark.driver.memory", "1g") \
+    .config("spark.sql.adaptive.enabled", "true") \
+    .getOrCreate()
 # %%
 # %%
-from pyspark.sql import SparkSession
 dados = (spark.read
          .format("csv")
          .option("inferSchema", "false")
