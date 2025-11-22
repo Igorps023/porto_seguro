0b831e8743ddfe72e800f9900194397f256834c0
Igor <you@example.com>
2025-11-22 15:02:30 -0300

    novos scripts

---
+++ main.py
@@ -0,0 +1,27 @@
+# %%
+from pyspark.sql import functions as F
+from sklearn import tree
+
+
+# %%
+
+spark = SparkSession.builder \
+    .appName("MyApp") \
+        .config("spark.executor.memory", "2g") \
+.config("spark.driver.memory", "1g") \
+            .config("spark.sql.adaptive.enabled", "true") \
+        .getOrCreate()
+# %%
+# %%
+from pyspark.sql import SparkSession
+dados = (spark.read
+         .format("csv")
+         .option("inferSchema", "false")
+         .load("data/train.csv")
+         )
+# %%
+amostra = dados.limit(10)
+# %%
+amostra_df = amostra.toPandas()
+# %%
+tree.DecisionTreeClassifier()
